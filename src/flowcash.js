1748a821a295ac0228685e88887e26e2adbdb887
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-03 02:01:49 -0700

    add async to flowcash.js

---
+++ src/flowcash.js
@@ -0,0 +1,16 @@
+export class flowCash {
+  async getCurrency() {
+    try {
+      let response = await fetch(`https://prime.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      return false;
+    }
+  }
+}
