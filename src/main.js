05e94bb3c59661518b9c4136ffedc7c9ef8a0d04
blacchoy@gmail.com <blacchoy@gmail.com>
2020-07-03 01:54:19 -0700

    start user & business logic in main.js

--- src/main.js
+++ src/main.js
@@ -2,3 +2,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { flowCash } from "./../src/flowcash.js";
+
+$(document).ready(function() {
+
+  $('#form').submit(function(event) {
+    event.preventDefault();
+    const amount = $("#usd").val();
+    const convertCurrency = $("#convert").val();
+
+    (async () => {
+      let currencyExchanger = new flowCash();
+      const response = await currencyExchanger.getCurrency();
+      getConversion(response);
+    })();
+
+    function getConversion(response) {
+      if (response.conversion_rates[convertCurrency]) {
+        $('#output').text(`Your USD ${amount} is`);
+      } else {
+        $('#output').text(`There was an error handling your request.`);
+      }
+    }
+  });
+});
